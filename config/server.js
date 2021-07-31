0ca81b24368dbe4fc967649c3d0b34ac68feac57
mo3ty <seso979@hotmail.com>
2021-07-31 02:37:27 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '68f24e9d8782bc2355cbb7fba9e1e419'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1094df8d-492a-41d2-a3af-4d59f8d756f5'
+};
