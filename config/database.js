f81d3bb3fc815edde1cae808dd8828beeb2abfb9
mo3ty <seso979@hotmail.com>
2021-08-22 02:40:26 +0200

    Updated Yarn lockfile

--- config/database.js
+++ config/database.js
@@ -1,3 +1,5 @@
+
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -18,3 +20,26 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+/*
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST','cluster0.evtcr.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME','my-shibin'),
+        username: env('DATABASE_USERNAME','admin'),
+        password: env('DATABASE_PASSWORD' ,'@abdelmo3ty16'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
+*/
